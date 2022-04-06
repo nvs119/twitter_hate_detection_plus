78f8ec62d0442ff545c280340a7b9e9806344367
Niva Sivakumar <blueflute19@gmail.com>
2022-04-05 22:38:21 -0400

    Pagerank basically done but not cleaned up, bar graphs good

--- run_pagerank.py
+++ run_pagerank.py
@@ -1,7 +1,8 @@
-import matplotlib
+import matplotlib.pyplot as plt
 # from pygraphml import GraphMLParser
 # from pygraphml import Graph
 import networkx as nx
+import numpy as np
 import csv
 
 import os
@@ -28,11 +29,19 @@ pr = nx.pagerank(g)
 
 sorted_pr = {k: v for k, v in sorted(pr.items(), key=lambda item: item[1])}
 
+TOP_HOW_MANY = 100
+
 count = 0
 hateful = 0
 normal = 0
 avg_hateful = 0
 avg_normal = 0
+hateful_ranks = []
+normal_ranks = []
+
+hateful_bar_graph = [0, 0, 0, 0]
+normal_bar_graph = [0, 0, 0, 0]
+
 for item in sorted_pr:
     if users[item] == "other":
         continue
@@ -42,22 +51,51 @@ for item in sorted_pr:
         if users[item] == "hateful":
             hateful += 1
             avg_hateful += count
+            hateful_ranks.append(count)
+            if (count / TOP_HOW_MANY) >= 0.75:
+                hateful_bar_graph[3] += 1
+            elif (count / TOP_HOW_MANY) >= 0.5:
+                hateful_bar_graph[2] += 1
+            elif (count / TOP_HOW_MANY) >= 0.25:
+                hateful_bar_graph[1] += 1
+            else:
+                hateful_bar_graph[0] += 1
         elif users[item] == "normal":
             normal += 1
             avg_normal += count
+            normal_ranks.append(count)
+            if (count / TOP_HOW_MANY) >= 0.75:
+                normal_bar_graph[3] += 1
+            elif (count / TOP_HOW_MANY) >= 0.5:
+                normal_bar_graph[2] += 1
+            elif (count / TOP_HOW_MANY) >= 0.25:
+                normal_bar_graph[1] += 1
+            else:
+                normal_bar_graph[0] += 1
         count += 1
-    if count == 100:
+    if count == TOP_HOW_MANY:
         break
 
-print("Number of top 100 infuential users that are hateful: " + str(hateful))
-print("Number of top 100 influential users that are normal: " + str(normal))
+print("Number of top " + str(TOP_HOW_MANY) + " infuential users that are hateful: " + str(hateful))
+print("Number of top " + str(TOP_HOW_MANY) + " influential users that are normal: " + str(normal))
 
 # is this a good metric to have? when there are roughly 9x more normal users
 # than hateful?
-print("Average pagerank of a top 100 hateful user: " + str(avg_hateful / hateful))
-print("Average pagerank of a top 100 normal user: " + str(avg_normal / normal))
+# print("Average pagerank of a top 100 hateful user: " + str(avg_hateful / hateful))
+# print("Average pagerank of a top 100 normal user: " + str(avg_normal / normal))
 
 # line graph
 # plot normal users by page rank
 # plot hateful users by page rank
+x_percentiles = ["top 25%", "25% - 50%", "50% - 75%", "75% - 100%"]
+x_axis = np.arange(len(x_percentiles))
+hateful_bar_graph = [(i / hateful) * 100 for i in hateful_bar_graph]
+normal_bar_graph = [(j / normal) * 100 for j in normal_bar_graph]
+
+plt.bar(x_axis - 0.2, hateful_bar_graph, width=0.4, label = "Hateful")
+plt.bar(x_axis + 0.2, normal_bar_graph, width=0.4, label = "Normal")
+
+plt.xticks(x_axis, x_percentiles)
+plt.legend()
+plt.show()
 
